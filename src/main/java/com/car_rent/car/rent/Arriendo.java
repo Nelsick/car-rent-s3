23a8cdf44293715c74904855988668bdd03dff39
Nelsick D <yiris@outlook.cl>
2022-11-04 18:04:24 -0300

    Creados los objetos arriendo y devolucion

---
+++ src/main/java/com/car_rent/car/rent/Arriendo.java
@@ -0,0 +1,11 @@
+package com.car_rent.car.rent;
+
+import java.util.Date;
+
+public class Arriendo {
+    private Integer num_arr;
+    private Vehiculo vehiculo;
+    private Cliente cliente;
+    private Date fec_arr;
+    private Integer dias_arr;
+}
--- src/main/java/com/car_rent/car/rent/CarREnt.java
+++ src/main/java/com/car_rent/car/rent/CarREnt.java
@@ -8,8 +8,5 @@ public class CarREnt {
 
     public static void main(String[] args) {
         Cliente cliente = new Cliente("18170778-K","Yiris Majluf");
-        cliente.setCedula("18170778-K");
-        System.out.println(cliente.getCedula());
-        System.out.println(cliente.getVigencia());
     }
 }
