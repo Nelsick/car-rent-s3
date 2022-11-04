0dd5ccd495cfabd689186f0be59920802138ef53
Nelsick D <yiris@outlook.cl>
2022-11-04 19:44:08 -0300

    Se crea la funcion para evaluar un arriendo

--- src/main/java/com/car_rent/car/rent/Arriendo.java
+++ src/main/java/com/car_rent/car/rent/Arriendo.java
@@ -1,15 +1,15 @@
 package com.car_rent.car.rent;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 public class Arriendo {
     private Integer num_arr;
     private Vehiculo vehiculo;
     private  Cliente cliente;
-    private Date fec_arr;
+    private LocalDate fec_arr;
     private Integer dias_arr;
 
-    public Arriendo(Integer num_arr, Vehiculo vehiculo, Cliente cliente, Date fec_arr, Integer dias_arr) {
+    public Arriendo(Integer num_arr, Vehiculo vehiculo, Cliente cliente, LocalDate fec_arr, Integer dias_arr) {
         this.num_arr = num_arr;
         this.vehiculo = vehiculo;
         this.cliente = cliente;
@@ -41,11 +41,11 @@ public class Arriendo {
         this.cliente = cliente;
     }
 
-    public Date getFec_arr() {
+    public LocalDate getFec_arr() {
         return fec_arr;
     }
 
-    public void setFec_arr(Date fec_arr) {
+    public void setFec_arr(LocalDate fec_arr) {
         this.fec_arr = fec_arr;
     }
 
@@ -55,5 +55,18 @@ public class Arriendo {
 
     public void setDias_arr(Integer dias_arr) {
         this.dias_arr = dias_arr;
+    }
+
+    public boolean check_rent(){
+        if(this.cliente.getVigencia() && (this.vehiculo.getCondicion() == "D")){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public String toString(){
+        return "El vehiculo: " + vehiculo.getPatente() + " se ha reservado correctamente por "+ getDias_arr() + " días a fecha: " + getFec_arr();
     }
 }
--- src/main/java/com/car_rent/car/rent/CarREnt.java
+++ src/main/java/com/car_rent/car/rent/CarREnt.java
@@ -4,12 +4,15 @@
 
 package com.car_rent.car.rent;
 
+import java.time.LocalDate;
+
 public class CarREnt {
 
     public static void main(String[] args) {
         Cliente cliente = new Cliente("18170778-K","Yiris Majluf");
-        cliente.setCedula("18170778-K");
-        System.out.println(cliente.getCedula());
-        System.out.println(cliente.getVigencia());
+        Vehiculo vehiculo = new Vehiculo("LYDX-79","CHANGAN","NEW CS15",2022,"D");
+        Arriendo arriendo = new Arriendo(1,vehiculo,cliente, LocalDate.now(),3);
+        System.out.println(arriendo.check_rent());
+        System.out.println(arriendo.toString());
     }
 }
