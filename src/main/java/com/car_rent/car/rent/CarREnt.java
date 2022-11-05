7190c1fe2641ac3421225ec49014a121d2954d47
Anellisse <cdvidal@uc.cl>
2022-11-04 23:01:21 -0300

    Actualización clase devolucion

--- src/main/java/com/car_rent/car/rent/CarREnt.java
+++ src/main/java/com/car_rent/car/rent/CarREnt.java
@@ -6,18 +6,32 @@ package com.car_rent.car.rent;
 
 import java.time.LocalDate;
 
+
 public class CarREnt {
 
     public static void main(String[] args) {
         Cliente cliente = new Cliente("18170778-K","Yiris Majluf");
         Vehiculo vehiculo = new Vehiculo("LYDX-79","CHANGAN","NEW CS15",2022);
-        Arriendo arriendo = new Arriendo(1,vehiculo,cliente, LocalDate.now(),3);
-        System.out.println(vehiculo.toString());
-        System.out.println(arriendo.taken_rent());
-        System.out.println(vehiculo.toString());
+        Arriendo changan = new Arriendo(1,vehiculo,cliente, LocalDate.now(),3);
 
-        int valor_arriendo = arriendo.rent_amount();
+        /*
+        System.out.println(vehiculo.toString());
+        System.out.println(changan.taken_rent());
+        System.out.println(vehiculo.toString());
+        int valor_arriendo = changan.rent_amount();
         System.out.println(valor_arriendo);
-        System.out.println(arriendo.rent_ticket());
+        System.out.println(changan.rent_ticket());
+         */
+
+        Devolucion devolucion = new Devolucion(changan, "2022-11-03");
+        System.out.println(devolucion.toString());
+
+        devolucion.setFec_dev("2022-11-02");
+        System.out.println(devolucion.toString());
+
+        System.out.println(devolucion.car_return());
+
+
+
     }
 }
