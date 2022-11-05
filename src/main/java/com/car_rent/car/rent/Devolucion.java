327cf6fff1274256313f6f1c8fb13375dd15efc1
Anellisse <cdvidal@uc.cl>
2022-11-04 21:48:27 -0300

    Clase devolución en construcción

--- src/main/java/com/car_rent/car/rent/Devolucion.java
+++ src/main/java/com/car_rent/car/rent/Devolucion.java
@@ -1,4 +1,16 @@
 package com.car_rent.car.rent;
 
+import java.time.LocalDate;
+
 public class Devolucion {
+    private Arriendo arriendo;
+    private LocalDate fec_dev;
+
+    public Devolucion(Arriendo arriendo, LocalDate fec_dev){
+        this.arriendo = arriendo;
+        this.fec_dev = fec_dev;
+    }
 }
+
+
+
